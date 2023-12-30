3f54f5139d5205f1533a1009682380d530a56746
shirley <19404426+BlenderWang@users.noreply.github.com>
2023-12-30 16:14:14 +0100

    create react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import logo from './logo.svg';
+import './App.css';
 
-export default () => {
-	return <div>
-		<h1>Hi there!!!</h1>
-		<hr/>
-		<MarketingApp/>
-	</div>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
